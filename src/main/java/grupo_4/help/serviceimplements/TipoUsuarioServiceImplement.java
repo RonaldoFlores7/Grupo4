58c441366ae76f70c483ab9991aafab2d56227e0
RonaldoF17 <fc0498687@gmail.com>
2024-08-28 02:52:58 -0500

    Primer Commit

---
+++ src/main/java/grupo_4/help/serviceimplements/TipoUsuarioServiceImplement.java
@@ -0,0 +1,33 @@
+package grupo_4.help.serviceimplements;
+
+import grupo_4.help.entities.TipoUsuario;
+import grupo_4.help.repositories.ITipoUsuarioRepository;
+import grupo_4.help.serviceinterfaces.ITipoUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class TipoUsuarioServiceImplement implements ITipoUsuarioService {
+    @Autowired
+    private ITipoUsuarioRepository tuR;
+
+    @Override
+    public List<TipoUsuario> list() {return tuR.findAll();}
+
+    @Override
+    public void insert(TipoUsuario tipoUsuario) {
+        tuR.save(tipoUsuario);
+    }
+
+    @Override
+    public void update(TipoUsuario tipoUsuario) {
+        tuR.save(tipoUsuario);
+    }
+
+    @Override
+    public void delete(int idTipoUsuario) {
+        tuR.deleteById(idTipoUsuario);
+    }
+
+}

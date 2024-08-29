59afc6e621335bf5738e8ff9795d952f116b7f12
DaysyF-21 <daysyf2108@gmail.com>
2024-08-28 22:47:36 -0500

    CRUD Tipo de Campañas

--- src/main/java/grupo_4/help/repositories/ITipoCampaniaRepository.java
+++ src/main/java/grupo_4/help/repositories/ITipoCampaniaRepository.java
@@ -1,9 +1,10 @@
 package grupo_4.help.repositories;
 
-import grupo_4.help.entities.CampaniaDonaciones;
+import grupo_4.help.entities.TipoCampania;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICamapaniaDonacionesRepository extends JpaRepository<CampaniaDonaciones, Integer> {
+public interface ITipoCampaniaRepository extends JpaRepository<TipoCampania, Integer> {
+
 }

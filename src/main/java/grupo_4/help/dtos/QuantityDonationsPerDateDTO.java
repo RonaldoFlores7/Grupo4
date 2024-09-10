165119f2251f62f76db7b9001414236de2b6cb6b
SebastianCruz10 <sebascruz.0410@gmail.com>
2024-09-10 00:51:08 -0500

    Merge remote-tracking branch 'origin/Sebastian' into Sebastian

---
+++ src/main/java/grupo_4/help/dtos/QuantityDonationsPerDateDTO.java
@@ -0,0 +1,22 @@
+package grupo_4.help.dtos;
+
+public class QuantityDonationsPerDateDTO {
+    public int idDonacion;
+    public int cantidadDonacionesPorFecha;
+
+    public int getIdDonacion() {
+        return idDonacion;
+    }
+
+    public void setIdDonacion(int idDonacion) {
+        this.idDonacion = idDonacion;
+    }
+
+    public int getCantidadDonacionesPorFecha() {
+        return cantidadDonacionesPorFecha;
+    }
+
+    public void setCantidadDonacionesPorFecha(int cantidadDonacionesPorFecha) {
+        this.cantidadDonacionesPorFecha = cantidadDonacionesPorFecha;
+    }
+}

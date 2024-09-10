4776085a6e4ae4c15012a3c358433964d7a0eb65
David Canal Castillo <u202221767@upc.edu.pe>
2024-09-09 22:14:54 -0500

    Update IDistritoRepository.java
    faltaba el estereotipo '@Repository'

--- src/main/java/grupo_4/help/repositories/IDistritoRepository.java
+++ src/main/java/grupo_4/help/repositories/IDistritoRepository.java
@@ -2,6 +2,8 @@ package grupo_4.help.repositories;
 
 import grupo_4.help.entities.Distrito;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IDistritoRepository extends JpaRepository<Distrito, Integer> {
 }

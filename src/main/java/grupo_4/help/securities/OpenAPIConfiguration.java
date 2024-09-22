0587c3bad872a2945252539208bebe1b7d08e211
RonaldoF17 <u202122766@upc.edu.pe>
2024-09-22 15:12:45 -0500

    OPENAPI + SWAGGER

--- src/main/java/grupo_4/help/securities/OpenAPIConfiguration.java
+++ src/main/java/grupo_4/help/securities/OpenAPIConfiguration.java
@@ -13,5 +13,13 @@ public class OpenAPIConfiguration {
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
 }
